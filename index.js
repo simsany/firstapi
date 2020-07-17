224558f4249f1305fd3eff72f6ab7fd44a7163d2
simsany <simsany@.com>
2020-07-17 17:10:26 +0200

    try

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ var app=express();
 var mongoose=require("mongoose");
 var routes=require("./routes/todo")
 var bodyParser=require("body-parser");
+const { expressCspHeader, NONCE } = require('express-csp-header');
+ 
+app.use(expressCspHeader({
+    directives: {
+        'script-src': [NONCE]
+    }
+}));
+
+
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 var mongoose=require("mongoose");
 mongoose.set('debug',true);
 
-mongoose.connect("mongodb+srv://sanyika:Nem99@cluster0.hg5xp.mongodb.net/orders?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://sanyika:Nem99@cluster0.hg5xp.mongodb.net/orders?retryWrites=true&w=majority");
+
 mongoose.Promise=Promise;
 module.exports.Todo=require("./todo")
